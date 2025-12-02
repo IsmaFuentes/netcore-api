6fe044c99bb3b251dba5d26063aaa607b98d647d
IsmaFuentes <ifuentes@dispreu.com>
2025-12-02 10:43:20 +0100

    testing project

---
+++ netcore-api.Test/UnitTesting.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace netcore_api.Test
+{
+  public class UnitTesting
+  {
+    [Fact]
+    public async Task Test_GetUserAsync()
+    {
+      var options = new DbContextOptionsBuilder<Data.Context>()
+        .UseInMemoryDatabase(databaseName: "TestingDb").Options;
+
+      var context = new Data.Context(options);
+
+      context.Users.Add(new Data.Entities.User()
+      {
+        UserName = "Administrator",
+        Password = "password",
+        Role = Data.Entities.UserRole.Admin,
+        IsActive = true,
+      });
+
+      await context.SaveChangesAsync();
+
+      var logger = new Mock<ILogger<Services.UserService>>();
+      var hasher = new Mock<IPasswordHasher<Data.Entities.User>>();
+
+      var service = new Services.UserService(context, hasher.Object, logger.Object);
+
+      var result = await service.GetUserAsync(1);
+      Assert.NotNull(result);
+      Assert.Equal("Administrator", result.UserName);
+    }
+  }
+}
